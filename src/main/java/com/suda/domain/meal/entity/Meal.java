89a5cf654c190edc177d3c0e9150a14da1c27f77
kimhoeyun <clnvrd@suwon.ac.kr>
2025-10-04 15:54:23 +0900

    refactor: Meal 엔티티 단순화를 위해 Weekday 엔티티 제거 및 리팩토링

--- src/main/java/com/suda/domain/meal/entity/Meal.java
+++ src/main/java/com/suda/domain/meal/entity/Meal.java
@@ -3,43 +3,34 @@ package com.suda.domain.meal.entity;
 import com.suda.domain.cafeteria.entity.Cafeteria;
 import com.suda.domain.weekday.entity.Weekday;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Meal {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cafeteria_id")
     private Cafeteria cafeteria;
 
-    @ManyToOne
-    @JoinColumn(name = "weekday_id")
-    private Weekday weekday;
-
-    private String mealType;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DayOfWeek dayOfWeek;
 
     @Column(columnDefinition = "TEXT")
     private String menu;
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    public Meal(Cafeteria cafeteria, Weekday weekday, String mealType, String menu) {
+    public Meal(Cafeteria cafeteria, DayOfWeek dayOfWeek, String menu) {
         this.cafeteria = cafeteria;
-        this.weekday = weekday;
-        this.mealType = mealType;
+        this.dayOfWeek = dayOfWeek;
         this.menu = menu;
-    }
-
-    public Meal() {
-
     }
 }
