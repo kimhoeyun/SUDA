9b557d98b6c0af8b3579167fa2ddd85417974ae7
kimhoeyun <clnvrd@suwon.ac.kr>
2025-12-31 16:10:51 +0900

    refactor: 요일별 학식 제공 API의 응답 포맷을 오늘의 학식 API와 통일

--- src/main/java/com/suda/domain/meal/repository/MealRepository.java
+++ src/main/java/com/suda/domain/meal/repository/MealRepository.java
@@ -5,7 +5,11 @@ import com.suda.domain.meal.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
     List<Meal> findAllByDayOfWeek(DayOfWeek dayOfWeek);
+
+    // 종강(=종합강의동) / 아마랜스 오늘 메뉴를 각각 가져오기
+    Optional<Meal> findByCafeteria_NameAndDayOfWeek(String cafeteriaName, DayOfWeek dayOfWeek);
 }
