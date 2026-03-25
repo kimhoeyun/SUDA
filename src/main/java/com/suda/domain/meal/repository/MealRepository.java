e40713b21617aba853301cfdcf3d10fab3600162
hanjh <wg1004s@naver.com>
2026-03-25 16:16:06 +0900

    fix: 학식 조회 시 식당 지연 로딩 쿼리 제거

--- src/main/java/com/suda/domain/meal/repository/MealRepository.java
+++ src/main/java/com/suda/domain/meal/repository/MealRepository.java
@@ -3,11 +3,18 @@ package com.suda.domain.meal.repository;
 import java.time.DayOfWeek;
 import com.suda.domain.meal.entity.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
+    @Query("""
+            select m
+            from Meal m
+            join fetch m.cafeteria
+            where m.dayOfWeek = :dayOfWeek
+            """)
     List<Meal> findAllByDayOfWeek(DayOfWeek dayOfWeek);
 
     // 종강(=종합강의동) / 아마랜스 오늘 메뉴를 각각 가져오기
